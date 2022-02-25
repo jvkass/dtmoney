5a476336e46d545b0f3b8afaa9c2a2cfa95fba8a
jvkass <jvcnmata@gmail.com>
2022-02-25 20:14:55 -0300

    Criação do formulários e estilização do modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,17 +1,38 @@
 import Modal from "react-modal";
+import { Container } from "./styles";
 
 interface NewTransactionModalProps {
     onIsOpen: boolean;
-    onIsRequestClose:()=>void;
+    onIsRequestClose: () => void;
 }
 
-export function NewTransactionModal({onIsOpen, onIsRequestClose}:NewTransactionModalProps){
-   return(
-    <Modal
-    isOpen={onIsOpen}
-    onRequestClose={onIsRequestClose}
-  >
-    <h2>Cadastrar transação</h2>
-  </Modal>
-   ); 
+export function NewTransactionModal({ onIsOpen, onIsRequestClose }: NewTransactionModalProps) {
+    return (
+        <Modal
+            isOpen={onIsOpen}
+            onRequestClose={onIsRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
+        >
+
+            <Container>
+                <h2>Cadastrar transação</h2>
+
+                <input
+                    placeholder="Titulo"
+                />
+
+                <input
+                    type="number"
+                    placeholder="Valor"
+                />
+
+                <input
+                    placeholder="Categoria"
+                />
+
+                <button placeholder="submit">Cadastrar</button>
+            </Container>
+        </Modal>
+    );
 }
