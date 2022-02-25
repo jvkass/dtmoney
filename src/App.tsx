36b6b2481bb74fd11491cb0ba882d6b38246d8ce
jvkass <jvcnmata@gmail.com>
2022-02-25 12:42:15 -0300

    Styled Global

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  font-size:64px;
-  color: #8257e6;
-`
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
-      <Title>
-        <h1>Hello world</h1>
-      </Title>
+      <h1>Hello world</h1>
+      <GlobalStyle/>
     </div>
   );
 }
