a3aa4f0028b6896219b0e24d49301290ec8f1b43
jvkass <jvcnmata@gmail.com>
2022-03-01 11:38:16 -0300

    Carregando transações com ContextAPI

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import totalImg from "../../assets/total.svg";
@@ -7,8 +7,9 @@ import { TransactionsContext } from "../../TransactionsContext";
 import { Container } from "./styles";
 
 export function Summary(){
-const data = useContext(TransactionsContext);
-console.log(data);
+const transactions = useContext(TransactionsContext);
+
+console.log(transactions);
 
     return(
         <Container>
