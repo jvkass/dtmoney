05490492d2695ee511d4619e5134968b006e9c6c
jvkass <jvcnmata@gmail.com>
2022-03-01 12:11:47 -0300

    Criando Transaction no ContextAPI

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,30 +3,31 @@ import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg"
 import outcomeImg from "../../assets/outcome.svg"
-import { FormEvent, useState } from "react";
+import { FormEvent, useState, useContext } from "react";
 import { api } from "../../services/api";
+import { TransactionsContext } from "../../TransactionsContext";
 interface NewTransactionModalProps {
     onIsOpen: boolean;
     onIsRequestClose: () => void;
 }
 
 export function NewTransactionModal({ onIsOpen, onIsRequestClose }: NewTransactionModalProps) {
+    const {createTransaction} = useContext(TransactionsContext);
+    
     const [title,setTitle] = useState('');
-    const [value,setValue] = useState(0);
-    const [category,setCategory] = useState(0);
+    const [amount,setAmount] = useState(0);
+    const [category,setCategory] = useState('');
     const [type,setType] = useState('deposit');
     
     function handleCreateNewTransaction(event: FormEvent){
         event.preventDefault(); 
 
-        const data ={
+        createTransaction({
             title,
-            value,
+            amount,
             category,
-            type
-        };
-
-        api.post('/transactions',data)
+            type,
+        });
     }
 
     return (
@@ -53,8 +54,8 @@ export function NewTransactionModal({ onIsOpen, onIsRequestClose }: NewTransacti
                 <input
                     type="number"
                     placeholder="Valor"
-                    value={value}
-                    onChange={e => setValue(Number(e.target.value))}
+                    value={amount}
+                    onChange={e => setAmount(Number(e.target.value))}
                 />
 
                 <TransactionTypeContainer>
@@ -81,7 +82,7 @@ export function NewTransactionModal({ onIsOpen, onIsRequestClose }: NewTransacti
                 <input
                     placeholder="Categoria"
                     value={category}
-                    onChange={e => setCategory(Number(e.target.value))}
+                    onChange={e => setCategory(e.target.value)}
                 />
 
                 <button type="submit">Cadastrar</button>
